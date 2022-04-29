ad57fd652fbfee4e56d51c61948d364a0eb722b6
Oliver Wilson <oliver.wilson96@hotmail.com>
2022-04-29 15:06:03 +0100

    added print to file

--- connect.py
+++ connect.py
@@ -1,6 +1,7 @@
 import socket, ssl
 import time
 import datetime
+from traceback import print_tb
 
 context = ssl.SSLContext()
 context.options |= ssl.OP_NO_TICKET #diable session ticket
@@ -12,14 +13,27 @@ s.settimeout(5) # set timeout
 sslSocket = context.wrap_socket(s, server_hostname = dn) # wrap socket into TLS context
 
 try:
+    
     begin = time.process_time() # start timer
     sslSocket.connect((dn, 443)) # TLS socket connection
     perf = time.process_time() - begin # end timer
 
     print ("Success! Performance time for TLS connection is: ", perf*1000) #convert from sec. to ms by multiplying with 1000
+    #with open(r"PythonResults.txt", 'r') as fp:
+    #    for count, line in enumerate(fp):
+    #        pass
+    #print('Total Lines', count + 1)
+    f = open("PythonResults.txt", "a")
+    perf = repr(perf*1000)
+    #count = repr(count)
+    f.write(perf)
+    f.write("\n")
+    f.close
 
 
 except (ssl.SSLError, ssl.SSLEOFError, ssl.CertificateError,ssl.SSLSyscallError, ssl.SSLWantWriteError, ssl.SSLWantReadError,ssl.SSLZeroReturnError) as e1:
+    print(e1)
     sslSocket.close() # close the socket
 except Exception as e2:
+    print(e2)
     sslSocket.close() # close the socket 
