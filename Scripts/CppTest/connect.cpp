012af912936f2c5fe181005213fdd579d67afa5d
Oliver Wilson <oliver.wilson96@hotmail.com>
2022-05-06 10:38:54 +0100

    C++ working

--- Scripts/CppTest/connect.cpp
+++ Scripts/CppTest/connect.cpp
@@ -6,26 +6,7 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
-//Not sure what headers are needed or not
-//This code (theoretically) writes "Hello World, 123" to a socket over a secure TLS connection
-//compiled with g++ -Wall -o client.out client.cpp -L/usr/lib -lssl -lcrypto
-//Based off of: https://www.cs.utah.edu/~swalton/listings/articles/ssl_client.c
-//The OpenConnection method was heavily based on the answer to this post: https://stackoverflow.com/questions/52727565/client-in-c-use-gethostbyname-or-getaddrinfo
-
 const int ERROR_STATUS = -1;
-
-SSL_CTX *InitSSL_CTX(void)
-{
-    const SSL_METHOD *method = TLS_client_method(); /* Create new client-method instance */
-    SSL_CTX *ctx = SSL_CTX_new(method);
-
-    if (ctx == nullptr)
-    {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
-    }
-    return ctx;
-}
 
 int OpenConnection(const char *hostname, const char *port)
 {
@@ -78,29 +59,10 @@ int OpenConnection(const char *hostname, const char *port)
     return sfd;
 }
 
-void DisplayCerts(SSL *ssl)
-{
-    X509 *cert = SSL_get_peer_certificate(ssl); /* get the server's certificate */
-    if (cert != nullptr)
-    {
-        printf("Server certificates:\n");
-        char *line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
-        printf("Subject: %s\n", line);
-        delete line;
-        line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
-        printf("Issuer: %s\n", line);
-        delete line;
-        X509_free(cert);
-    }
-    else
-    {
-        printf("Info: No client certificates configured.\n");
-    }
-}
-
 int main(int argc, char const *argv[])
 {
-    SSL_CTX *ctx = InitSSL_CTX();
+    const SSL_METHOD *method = TLS_client_method(); /* Create new client-method instance */
+    SSL_CTX *ctx = SSL_CTX_new(method);
     SSL *ssl = SSL_new(ctx);
     if (ssl == nullptr)
     {
@@ -109,7 +71,7 @@ int main(int argc, char const *argv[])
     }
 
     //Host is hardcoded to localhost for testing purposes
-    const int sfd = OpenConnection("127.0.0.1", argv[1]);
+    const int sfd = OpenConnection("oliverw14.pythonanywhere.com", "443");
     SSL_set_fd(ssl, sfd);
 
     const int status = SSL_connect(ssl);
@@ -122,8 +84,6 @@ int main(int argc, char const *argv[])
     }
 
     printf("Connected with %s encryption\n", SSL_get_cipher(ssl));
-    const char *chars = "Hello World, 123!";
-    SSL_write(ssl, chars, strlen(chars));
     SSL_free(ssl);
     close(sfd);
     SSL_CTX_free(ctx);
